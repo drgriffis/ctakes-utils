fde804d7a870789ebfd4e6e62e352afc49b13c2b
Denis Griffis <denisrgriffis@gmail.com>
2016-09-08 14:41:50 -0400

    Modified XMI.getTokens to support overlapping mentions

--- python/ctakes/annotations.py
+++ python/ctakes/annotations.py
@@ -8,3 +8,11 @@ class Mention:
         self.begin = bounds[0] if bounds != None else None
         self.end = bounds[1] if bounds != None else None
         self.text = None
+
+    def __repr__(self):
+        return '{ CUIs: [%s] Begin: %d End: %d Text: "%s" }' % (
+            ','.join(self.CUIs),
+            self.begin,
+            self.end,
+            self.text
+        )
--- python/ctakes/format/XMI.py
+++ python/ctakes/format/XMI.py
@@ -15,6 +15,14 @@ def getTokens(fpath, mentions=None):
     '''Get the ordered list of tokens from the document, as
     tokenized by cTAKES.
 
+    If list of Mentions is provided, the token list will consist
+    of the following types:
+     - str : non-mention token
+     - Mention : non-overlapping entity mention
+     - list : overlapping entity mentions within the same text span;
+              each item consists of non-mention tokens and a single Mention
+              e.g. [(Mention:"weight loss"), ("weight", Mention:"loss")]
+
     Parameters:
         fpath    :: path to XMI file to process
         mentions :: (optional) list of Mention objects to include
@@ -23,7 +31,12 @@ def getTokens(fpath, mentions=None):
     ### Assumes tokens of each type are stored in the file in sorted order.
 
     # if including mentions, index them by beginning index
-    if mentions != None: indexed_mentions = { m.begin:m for m in mentions }
+    # (may be multiple beginning at same index)
+    if mentions != None:
+        indexed_mentions = {}
+        for m in mentions:
+            if indexed_mentions.get(m.begin, None) == None: indexed_mentions[m.begin] = []
+            indexed_mentions[m.begin].append(m)
 
     # storage for instances of each token type
     tokens = [[] for _ in _token_regexes]
@@ -46,7 +59,11 @@ def getTokens(fpath, mentions=None):
     starts_remaining = []
     for i in range(len(starts)):
         if len(starts[i]) > 0: starts_remaining.append((i,starts[i]))
-    cur_mention = None
+    
+    cur_mentions = []   # current list of overlapping mentions
+    overlap_sofar = []  # tokens in the current overlap preceding the next mention
+    in_mention = False  # are we currently in >0 mentions?
+
     while len(starts_remaining) > 0:
         # find the next token type from the text
         next_starts = [start[0] for (_,start) in starts_remaining]
@@ -56,23 +73,37 @@ def getTokens(fpath, mentions=None):
         next_token = tokens[next_tokentype].pop(0)
         next_token_start = starts_remaining[next_starts_ix][1][0]
 
-        # if still in a mention
-        if cur_mention != None and cur_mention.end > next_token_start:
-            cur_mention.text.append(next_token)
-        # if just completed a mention
-        elif cur_mention != None and cur_mention.end <= next_token_start:
-            # flush the completed mention
-            cur_mention.text = ' '.join(cur_mention.text)
-            ordered_tokens.append(cur_mention)
-            cur_mention = None
-            # add the token
-            ordered_tokens.append(next_token)
+        # if still in one or more mentions
+        if in_mention:
+            # check for completed mentions and add token appropriately
+            in_mention = False
+            for (before, m, after) in cur_mentions:
+                if m.end > next_token_start:
+                    m.text.append(next_token)
+                    in_mention = True
+                else:
+                    after.append(next_token)
+
+            # if all mentions completed, flush them as a block
+            if not in_mention:
+                for (before, m, after) in cur_mentions:
+                    m.text = ' '.join(m.text)
+                    after.pop(-1) # the last token is spurious
+                ordered_tokens.append(cur_mentions)
+                # and reset the overlap trackers
+                cur_mentions, overlap_sofar = [], []
+
         # if starting a mention
-        elif mentions != None and indexed_mentions.get(next_token_start, None) != None:
-            cur_mention = indexed_mentions[next_token_start]
-            cur_mention.text = [next_token]
+        if mentions != None and indexed_mentions.get(next_token_start, None) != None:
+            new_mentions = []
+            for m in indexed_mentions[next_token_start]:
+                m.text = [next_token]
+                cur_mentions.append([ overlap_sofar.copy(), m, [] ])
+            overlap_sofar.append(next_token)
+            in_mention = True
+
         # otherwise, just add the word
-        else:
+        if not in_mention:
             ordered_tokens.append(next_token)
 
         # remove the starting index
@@ -84,11 +115,26 @@ def getTokens(fpath, mentions=None):
         starts_remaining = new_starts_remaining
 
     # check if still have a mention buffered
-    if cur_mention != None:
-        cur_mention.text = ' '.join(cur_mention.text)
-        ordered_tokens.append(cur_mention)
+    if in_mention:
+        for (before, m, after) in cur_mentions:
+            m.text = ' '.join(m.text)
+        ordered_tokens.append(cur_mentions)
 
-    return ordered_tokens
+    # flatten mentions and contexts
+    output_tokens = []
+    for t in ordered_tokens:
+        if type(t) == list:
+            if len(t) == 1:
+                (before, m, after) = t[0]
+                output_tokens.append(m)
+            else:
+                output_tokens.append([
+                    util.flatten([before, m, after])
+                    for (before, m, after) in t
+                ])
+        else: output_tokens.append(t)
+
+    return output_tokens
 
 def getMentions(fpath):
     '''Get the (ambiguous) entity mentions from the XMI file,
